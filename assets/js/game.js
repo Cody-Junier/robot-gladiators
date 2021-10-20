4445c706d43d4d019a52e13fdce3546d4316d111
Cody-Junier <90635812+Cody-Junier@users.noreply.github.com>
2021-10-20 12:56:40 -0500

    Completed 3.2.6

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,13 +3,13 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log (playerName, playerAttack, playerHealth);
+// console.log (playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack =12;
 
-var fight = function () {
+var fight = function (enemyName) {
     // Alert players that they are starting the round
     window.alert ("Welcome to Robot Gladiators!")
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -53,5 +53,25 @@ var fight = function () {
         window.alert("You need to choose a valid option. Try again!");
     }
     }
+for(var i = 0; i < enemyNames.length; i++){
+    fight(enemyNames[i]);
+}
+// fight();
 
-fight();
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//     *fight all enemy robots
+//     *Defeat all enemy robots
+// "LOSE" - Player robot's health is zero or less
+
+
+// console.log (enemyNames);
+// console.log(enemyNames[0]);
+// console.log(enemyNames[1]);
+// console.log(enemyNames[2]);
+// console.log(enemyNames.length);
+// for(var i = 0; i < enemyNames.length; i++) {
+//     console.log(enemyNames[i]);
+//     console.log(i);
+//     console.log(enemyNames[i] + " is at " + i + " index");
+//   }
